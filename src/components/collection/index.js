1e6d565b4c87746629bbad22560aa836fbdbe482
igao <nascimentoigor1311@gmail.com>
2023-12-24 19:07:52 -0300

    icones

--- src/components/collection/index.js
+++ src/components/collection/index.js
@@ -7,27 +7,51 @@ export default function Collection({ data }) {
                 <h2>{data.entity}</h2>
             </header>
 
-            <CollectionCells atributos={data.attributes}/>
+            <CollectionCells atributos={data.attributes} />
 
         </section>
     )
 
-    function CollectionCells({ atributos }) {
-        return atributos.map(prop => 
-            <div className="collection-field">
-                <div>
-                    <div className="name">
-                        <img src="/assets/images/simple_value.svg" alt="" />
-                        <h3>{prop.name}</h3>
+    function CollectionCells({ atributos, numero }) {
+        if(!numero) numero = 0;
+
+        return atributos.map(prop => {
+
+            let img = "/assets/images/icons/";
+
+            if(prop.type === "array_object") {
+                img = img + "array_object.svg";
+
+            } else if (prop.type.includes("array")) {
+                img = img + "array.svg";
+
+            } else if (prop.type === "object") {
+                img = img + "object.svg"
+
+            } else {
+                img = img + "simple_value.svg"
+            }
+
+            console.log(numero);
+
+            return (
+                <div className="collection-field">
+                    <div>
+                        <div className="name">
+                            <img src={img} alt="" />
+                            <h3>{prop.name}</h3>
+                        </div>
+
+                        <div className="type">
+                            <h4>{prop.type}</h4>
+                        </div>
                     </div>
 
-                    <div className="type">
-                        <h4>{prop.type}</h4>
-                    </div>
+                    {prop.attributes && <CollectionCells atributos={prop.attributes} numero={numero + 1}/>}
                 </div>
+            )
+        }
 
-                {prop.attributes && <CollectionCells atributos={prop.attributes}/>}
-            </div>
-        )    
+        )
     }
 }
