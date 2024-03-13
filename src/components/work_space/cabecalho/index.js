ece55272357301cc904f9aa3a8e0215b51665263
Igor <nascimentoigor1311@gmail.com>
2024-03-13 19:58:29 -0300

    componentização do titulo do projeto

--- src/components/work_space/cabecalho/index.js
+++ src/components/work_space/cabecalho/index.js
@@ -3,16 +3,11 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 
 import { get } from 'local-storage';
-import OutsideClickHandler from 'react-outside-click-handler';
-import callApi from '../../../api/callAPI';
-import { renameProject } from '../../../api/services/projectsAPI';
-import toast from 'react-hot-toast';
 import UserMenu from '../../account/user-menu';
+import ProjectName from './projectName';
 
 export default function Cabecalho({ projectInfo, permission }) {
 
-    const [name, setName] = useState('');
-    const [changeName, setChangeName] = useState(false);
     const [logged, setLogged] = useState(false);
 
     useEffect(() => {
@@ -23,22 +18,6 @@ export default function Cabecalho({ projectInfo, permission }) {
         }
     }, [])
 
-    useEffect(() => {
-        if (projectInfo)
-            setName(projectInfo.info.name);
-    }, [projectInfo])
-
-    useEffect(() => {
-        if (changeName) {
-            document.getElementById('change-name').select();
-        }
-    }, [changeName])
-
-    async function renameIt() {
-        await callApi(renameProject, projectInfo.id, name);
-        setChangeName(false);
-    }
-
     const navigate = useNavigate();
 
     return (
@@ -47,27 +26,7 @@ export default function Cabecalho({ projectInfo, permission }) {
                 <img src="/assets/images/logo.svg" alt="" />
             </div>
 
-            <div className='project-name'>
-                {
-                    projectInfo && changeName ?
-                        <OutsideClickHandler onOutsideClick={() => renameIt()}>
-                            <input type="text" value={name}
-                                id='change-name'
-                                onChange={e => setName(e.target.value)}
-                                onKeyDown={e => { if (e.key === 'Enter') renameIt() }} />
-                        </OutsideClickHandler> :
-
-                        <h2>{name ? name : 'Untitled'}</h2>
-                }
-
-                {
-                    projectInfo &&
-                    <button onClick={() => {permission === "owner" ? setChangeName(true) : toast("You don't have permission to change this.")}}>
-                        <img src="/assets/images/icons/chevron-down.svg" alt="" />
-                    </button>
-                }
-
-            </div>
+            <ProjectName project={projectInfo} permission={permission}/>
 
             <div className="user">
                 {!logged ?
